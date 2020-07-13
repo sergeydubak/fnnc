5209ff2ae048579b4c42eb7cb957f446b1eb9acc
Sergey Dubak <sergeydubak@yandex.com>
2020-07-13 18:09:19 +0300

    Add dj-database-url package

--- fnnc/settings.py
+++ fnnc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,6 +84,10 @@ DATABASES = {
         'PORT': os.environ.get('DATABASE_PORT', default='5432'),
     }
 }
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
